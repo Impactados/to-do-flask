47c05e5d382c883b73c512dff90d2532f065d458
Vmartinsz <vitormcamargo60@gmail.com>
2024-03-13 23:26:08 -0300

    criacao_tabela_task

--- app/database/db.sql
+++ app/database/db.sql
@@ -12,6 +12,19 @@ CREATE TABLE IF NOT EXISTS users (
     CONSTRAINT UC_User UNIQUE (id, username)
 );
 
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(50),
+    timer VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
 -- Substitua 'admin_password_hash' pelo hash SHA-256 real da senha desejada para o usuário admin.
 INSERT INTO users (name, username, password, active, age, gender, email) VALUES
 ('Admin', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', TRUE, 21, 'Não especificado', 'admin@admin.com');
